5fdd3d2ef3501755d448144f8d08cc27d104a0e7
5jisoo <56earls@gmail.com>
2023-02-05 02:08:56 +0900

    기본 API 이해

--- basic-security/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ basic-security/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,17 +1,26 @@
 package io.security.basicsecurity;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -22,71 +31,94 @@ import java.io.IOException;
 @Configuration
 // Web Security 애노테이션 설정 클래스, 여러 클래스들을 import해서 실행시켜주는 애노테이션. ==> 웹 보안 활성화를 위해 필수적.
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    UserDetailsService userDetailsService;
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("{noop}1111").roles("USER");
+        auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS");
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN");
+
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         // 인가 정책
         http
                 .authorizeRequests()    // 요청에 따른 보안검사 시작
+                .antMatchers("/login").permitAll()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/admin/pay").hasRole("ADMIN")
+                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
                 .anyRequest().authenticated();  // 어떠한 요청에도 인증을 받도록 설정.
 
         // 인증 정책
         http
                 .formLogin()
-//                .loginPage("/loginPage")
-//                .defaultSuccessUrl("/")
-//                .failureUrl("/login")
-//                .usernameParameter("userId")
-//                .passwordParameter("passwd")
-//                .loginProcessingUrl("/login_proc")
-//                .successHandler(new AuthenticationSuccessHandler() {    // 로그인에 성공한 경우 핸들러
-//                    @Override
-//                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-//                        System.out.println("authentication : " + authentication.getName());
-//                        response.sendRedirect("/"); // redirect
-//                    }
-//                })
-//                .failureHandler(new AuthenticationFailureHandler() {    // 로그인에 실패한 경우 핸들러
-//                    @Override
-//                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-//                        System.out.println("exception" + exception.getMessage());
-//                        response.sendRedirect("/loginPage");
-//                    }
-//                })
-//                .permitAll()    // "/loginPage"는 인증이 없어도 접근을 허락함.
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        RequestCache requestCache = new HttpSessionRequestCache();
+
+                        // 원래 사용자가 인증 전 가고자 했던 페이지가 저장되어 있음.
+                        SavedRequest savedRequest = requestCache.getRequest(request, response);
+                        String redirectUrl = savedRequest.getRedirectUrl();
+                        response.sendRedirect(redirectUrl); // 인증 성공 후 세션에 저장되어있던 이전의 요청 정보를 꺼내온 후 redirect
+                    }
+                })
         ;
 
-        http
-                .logout()
-//                .logoutUrl("/logout")   // default: /logout
-//                .logoutSuccessUrl("/login")   // 스프링 시큐리티에서는 로그아웃 처리를 POST 방식으로 해야 함.
-//                .addLogoutHandler(new LogoutHandler() {
+//        http
+//                .logout()
+//        ;
 //
-//                    // 원하는 기능 추가를 위해 직접 LogoutHandler()를 익명 클래스로 구현.
-//                    @Override
-//                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-//                        HttpSession session = request.getSession();
-//                        session.invalidate();   // 세션 무효화 작업
-//                    }
-//                })
-//                .logoutSuccessHandler(new LogoutSuccessHandler() {
-//                    @Override
-//                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-//                        response.sendRedirect("/login");    // 로그인 페이지로 이동하게 함.
-//                    }
-//                })
-//                .deleteCookies("remember-me")   // 서버에서 만든 쿠키명을 적어줌.
-        ;
+        /**
+         * Remember me 인증
+         */
+//        http
+//                .rememberMe()
+//                .userDetailsService(userDetailsService) // 미리 UserDetailsService를 Autowired 해줌.
+//        ;
 
+        /**
+         * 동시 세션 제어
+         */
+//        http
+//                .sessionManagement()
+//                .maximumSessions(1)
+//                .maxSessionsPreventsLogin(false)
+//        ;
+
+        /**
+         * 세션 고정 보호
+         */
+//        http
+//                .sessionManagement()
+//                .sessionFixation().changeSessionId()
+//        ;
+
+
+        /**
+         * 인증예외, 인가예외 처리
+         */
         http
-                .rememberMe()
-//                .rememberMeParameter("remember")
-//                .tokenValiditySeconds(3600)
-                .userDetailsService(userDetailsService) // 미리 UserDetailsService를 Autowired 해줌.
+                .exceptionHandling()
+                .authenticationEntryPoint(new AuthenticationEntryPoint() {
+                    @Override
+                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                        response.sendRedirect("/login");
+                    }
+                })
+                .accessDeniedHandler(new AccessDeniedHandler() {
+                    @Override
+                    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+                        response.sendRedirect("/denied");
+                    }
+                })
         ;
     }
 }
